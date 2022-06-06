abc8c72113536138f9df57b4f333e64bb85e9f88
Stan <ksstan@gmail.com>
2022-06-06 11:17:31 +0800

    Final commit - Course solution - The key difference between solutions being the random number is just generated once only

--- Resources/JS/js.js
+++ Resources/JS/js.js
@@ -1,12 +1,9 @@
-for (var i = 1; i >= 1; i++) {
-    
-    var randomNumbers = Math.floor(((Math.random()) * 10) + 1);
+var randomNumbers = Math.floor(((Math.random()) * 10) + 1);
+
+for (var i = 1; userNumber != randomNumbers; i++) {
 
     var userNumber = prompt("Please guess a number between 1 and 10..");
     userNumber = parseInt(userNumber);
-
-    if (userNumber === randomNumbers) {
-        alert("Well done! You have guessed the correct number " + randomNumbers + " in " + i + " number of attempts.");
-        break;
-    }
 }
+
+alert("Well done! You have guessed the correct number " + randomNumbers + " in " + i + " number of attempts.");
